b7155d5d026825a2ab3371e266ebab12809786bb
Ray Wu <rwu13795@gmail.com>
2021-03-11 12:23:06 -0500

    second part

--- index.js
+++ index.js
@@ -5,3 +5,14 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 
 app.use(morgan("dev"));
+app.use(express.static(__dirname + "/public"));
+
+app.get("/", (req, res) => {
+  res.send("");
+});
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+  console.log(`App listening in port ${PORT}`);
+});
